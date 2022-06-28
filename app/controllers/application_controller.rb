7fdac26b38167fcf8d61c6e5a261b48d0dd594ee
nicolas vassel <nicolas.vassel@gmail.com>
2022-06-29 00:02:27 +0100

    migration addlanguage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 end
 
-before_action ;set_local
+before_action :set_local
 
 def set_local
   if user_signed_in?
@@ -13,4 +13,9 @@ end
 
 def locale_from_header
   request.env.fetch('HTTP_ACCEPT_LANGUAGE', '').scan(/[a-z]{2}/).first
+end
+
+
+def configure_permitted_parameters
+  devise_parameter_meribel.permit([:language])
 end
