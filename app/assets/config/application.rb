816db888a989e599bc4334046303ea21d25fd69c
nicolas vassel <nicolas.vassel@gmail.com>
2022-06-28 23:51:48 +0100

    done

---
+++ app/assets/config/application.rb
@@ -0,0 +1,3 @@
+config.i18n.available_locales = [:en, :es, :fr, :ru]
+config.i18n.default_locale = :en
+config.i18n.fallbacks = true
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 end
+
+before_action ;set_local
+
+def set_local
+  if user_signed_in?
+    I18n.locale = current_user.language
+  else
+  I18n.locale = params[:lang] || locale_from_header || I18n.default_locale
+end
+
+def locale_from_header
+  request.env.fetch('HTTP_ACCEPT_LANGUAGE', '').scan(/[a-z]{2}/).first
+end
